f0aa384ff7ad7a8ed508a8c386a0fb14c95237bd
mmbukaneshy6 <sheilaegeidza@gmail.com>
2020-02-16 13:19:19 +0300

    added a js link

--- script.js
+++ script.js
@@ -7,4 +7,25 @@ submit.addEventListener("click", function() {
 
     var daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
+    var dd = document.getElementById("day").Value;
+    var mm = document.getElementById("month").value;
+    var yy = document.getElementById("year").value;
+    var date = new Date(yy + "-" + mm + "-" + dd);
+    var dayOfBirth = date.getDay();
+    var female = document.getElementById("female").value;
+    var male = document.getElementById("male").value;
+    var output = document.getElementById("output");
+
+    if (dd < 1 || dd > 31){
+        alert("oopsy");
+    }else if (mm < 1 || mm > 12){
+        alert("oopsy");
+    }else if(yy.toString().length !== 4){
+        alert("oopsy");
+    }else if (male.checked) {
+        output.innerHTML = "hello! you were born on" + daysOfWeek[dayOfBirth] + ", and your Akan Name is " + maleAkanNames[dayOfBirth];
+ }else{(female.checked)
+     output.innerHTML = "hello! you were born on" + daysOfWeek[daysOfBirth] + ", and your Akan Name is " + femaleAkanNames[dayOfBirth];
+ }
+
 })
