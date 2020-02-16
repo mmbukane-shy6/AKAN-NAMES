d4a5f55587f2d0e56a4a6b28b7cf533c68537060
mmbukaneshy6 <sheilaegeidza@gmail.com>
2020-02-16 11:54:17 +0300

    declared variables

---
+++ script.js
@@ -0,0 +1,10 @@
+var submit = document.getElementById("submit");
+submit.addEventListener("click", function() {
+
+    var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+    var femaleAkanNames = ["Akosua", "Adowa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    var daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+})
